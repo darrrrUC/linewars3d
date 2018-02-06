9a1b4d12c8559da3e02eb2cb77c5bc33ca23eb26
darrrrUC <ema_carlsson@yahoo.com>
2018-02-06 21:25:12 +0100

    INIT

---
+++ lanewars3d/Assets/scripts/moving/MoveToTarget.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToTarget : MonoBehaviour {
+
+    public Transform _target;
+    public float _speed;
+
+	void Start()
+    {
+        _target = GameObject.Find("Target").transform;
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(_target.position);
+    }
+}
---
+++ lanewars3d/Assets/scripts/spawning/ClickDetection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ClickDetection : NetworkBehaviour {
+
+    public GameObject _spawnUnit;
+    public Transform _spawnPoint;
+
+	void OnMouseDown()
+    {
+
+        Debug.Log("Attempting to spawn unity");
+        CmdSpawnUnit();
+    }
+
+    [Command]
+    private void CmdSpawnUnit()
+    {
+        _spawnPoint = GameObject.Find("spawnpoint").transform;
+        var spawnUnit = Instantiate(_spawnUnit, _spawnPoint.position, Quaternion.identity);
+        NetworkServer.Spawn(spawnUnit);
+    }
+}
